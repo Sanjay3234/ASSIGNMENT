9b6a544e61f1e3f7f55feb1b91309b92a16b83cd
sanjay3234 <sanjayapandit1111@gmail.com>
2019-11-18 22:07:33 +0545

    b

--- class6.py
+++ class6.py
@@ -34,7 +34,7 @@ def plaindrome(num):
     reverse=0
     while num>0:
         reminder=num%10
-        num=int(num/10)
+        num=num//10
         reverse=reverse*10+reminder
     if a==reverse:
         print(" is an armstrong number")
